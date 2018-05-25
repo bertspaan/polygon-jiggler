454b19de358f1b927aa0cffb86aa7c7086b82c5d
Bert Spaan <bert.spaan@gmail.com>
2018-05-25 14:58:41 +0200

    removed old code

--- src/index.js
+++ src/index.js
@@ -1,167 +1,10 @@
 const IMAGE_URL = '510d47e0-bf3e-a3d9-e040-e00a18064a99.jpg'
+const LINE_SAMPLE_RESOLUTION = 5
+const SEARCH_SIZE = 20
 
-//
-//
-// const url = '510d47e0-bf3e-a3d9-e040-e00a18064a99.jpg'
-//
-// const polygonCanvas = document.getElementById('polygon-canvas')
-// const polygonCtx = polygonCanvas.getContext('2d')
-//
-// const imageElement = document.getElementById('image')
-//
-// const img = new Image
-//
-// let imageCanvas
-//
-// let workerRunning = false
-//
-// let polygon = []
-// let drawing = true
-//
-// let worker
-//
-// function startJiggling () {
-//   worker = new Worker('worker.js')
-//
-//   worker.addEventListener('error', (event) => {
-//     console.error(event.message)
-//   })
-//
-//   worker.addEventListener('message', (event) => {
-//     if (event && event.data) {
-//       const results = event.data
-//
-//       polygon = event.data.polygon
-//       const scores = event.data.scores
-//       draw(polygon, scores)
-//     } else {
-//       workerRunning = false
-//     }
-//   })
-//
-//   const imageData = imageCanvas.getContext('2d').getImageData(0, 0, imageCanvas.width, imageCanvas.height)
-//   const buffer = imageData.data.buffer
-//
-//   worker.postMessage({
-//     stepCount: 1000,
-//     buffer,
-//     dimensions: [imageData.width, imageData.height],
-//     polygon
-//   }, [buffer])
-//
-//   workerRunning = true
-//   // window.requestAnimationFrame(step)
-// }
-//
-// // function step (timestamp) {
-// //   draw(polygon)
-// //
-// //   if (running) {
-// //     window.requestAnimationFrame(step)
-// //   }
-// // }
-//
-// img.addEventListener('load', () => {
-//   polygonCanvas.height = img.height
-//   polygonCanvas.width = img.width
-//
-//   polygonCanvas.style.height = img.height + 'px'
-//   polygonCanvas.style.width = img.width + 'px'
-//
-//   imageElement.src = url
-//
-//   imageCanvas = document.createElement('canvas')
-//   imageCanvas.width = img.width
-//   imageCanvas.height = img.height
-//   imageCanvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height)
-// })
-//
-// img.src = url
-//
-// function getLineColor (value, alpha=0.9) {
-//   const r = Math.round(255 * value)
-//   const g = Math.round(255 * (1 - value))
-//   const b = 0
-//   return `rgba(${r}, ${g}, ${b}, ${alpha})`
-// }
-//
-// function draw (polygon, scores) {
-//   polygonCtx.clearRect(0, 0, polygonCanvas.width, polygonCanvas.height)
-//
-//   polygonCtx.beginPath()
-//
-//   const lineWidth = 5
-//
-//   polygonCtx.lineWidth = lineWidth
-//   polygonCtx.fillStyle = 'red'
-//
-//   if (polygon.length > 1) {
-//     polygon.slice(1)
-//       .forEach((point, index) => {
-//         if (scores && scores[index]) {
-//           polygonCtx.strokeStyle = getLineColor(scores[index])
-//         } else {
-//           polygonCtx.strokeStyle = 'rgba(0, 0, 0, 0.5)'
-//         }
-//
-//         polygonCtx.beginPath()
-//
-//         const pointA = polygon[index]
-//         const pointB = polygon[index + 1]
-//
-//         polygonCtx.moveTo(pointA[0], pointA[1])
-//         polygonCtx.lineTo(pointB[0], pointB[1])
-//         polygonCtx.stroke()
-//       })
-//   }
-//
-//   polygonCtx.lineWidth = 0
-//   polygonCtx.fillStyle = 'black'
-//
-//   polygon.forEach((point) => {
-//     polygonCtx.beginPath()
-//     polygonCtx.arc(point[0], point[1], lineWidth * 2, 0, 2 * Math.PI)
-//     polygonCtx.fill()
-//   })
-//
-//   polygonCtx.stroke()
-// }
-//
-// function distance (pointA, pointB) {
-//   const dX = pointA[0] - pointB[0]
-//   const dY = pointA[1] - pointB[1]
-//   return Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2))
-// }
-//
-// const distanceThreshold = 20
-//
-// polygonCanvas.addEventListener('click', (event) => {
-//   if (!drawing) {
-//     return
-//   }
-//
-//   const point = [event.layerX, event.offsetY]
-//
-//   if (polygon.length >= 3 && distance(polygon[0], point) < distanceThreshold) {
-//     polygon.push(polygon[0])
-//     drawing = false
-//   } else {
-//     polygon.push(point)
-//   }
-//
-//   draw(polygon)
-// })
-//
-// document.getElementById('reset').addEventListener('click', () => {
-//   drawing = true
-//   polygon = []
-//   draw(polygon)
-// })
-//
-// document.getElementById('again').addEventListener('click', () => {
-//   startJiggling()
-// })
-
+const maxFailCount = 300
+const maxSteps = 1000
+const framerate = 30
 
 function pixelBrightness (pixel) {
   const r = pixel[0]
@@ -186,8 +29,12 @@ function getLineColor (value, alpha=0.9) {
 }
 
 function evaluateEdge (pointA, pointB, getPixel, fn, resolution) {
-
   const length = distance(pointA, pointB)
+
+  if (length === 0) {
+    return 0
+  }
+
   const minumumSamples = Math.floor(length / resolution)
 
   const usedResolution = length / (minumumSamples + 1)
@@ -210,11 +57,11 @@ function evaluateEdge (pointA, pointB, getPixel, fn, resolution) {
 }
 
 function evaluatePolygon (polygon, getPixel, fn, resolution) {
-  const edgeScores = Array(polygon.length - 1).fill(null)
+  const edgeScores = Array(polygon.length).fill(null)
     .map((u, i) => i)
     .map((index) => ([
       polygon[index],
-      polygon[index + 1]
+      polygon[(index + 1) % polygon.length]
     ]))
     .map((edge) => evaluateEdge(edge[0], edge[1], getPixel, fn, resolution))
 
@@ -229,24 +76,17 @@ function computeAverage (arr) {
   return arr.reduce((total, score) => total + score) / arr.length
 }
 
-const searchSize = 10
-const resolution = 5
-
-function searchStep (polygon, getPixel, fn, resolution) {
+function searchStep (polygon, dimensions, getPixel, fn, searchSize, resolution) {
   const currentEdgeScores = evaluatePolygon(polygon, getPixel, fn, resolution)
 
   const newPolygon = polygon.slice()
-  const moveIndex = getRandomIndex(newPolygon.length - 1)
+  const moveIndex = getRandomIndex(newPolygon.length)
   const movePoint = newPolygon[moveIndex]
 
   newPolygon[moveIndex] = [
-    movePoint[0] + searchSize * Math.random() - 0.5 * searchSize,
-    movePoint[1] + searchSize * Math.random() - 0.5 * searchSize
+    Math.min(Math.max(0, movePoint[0] + searchSize * Math.random() - 0.5 * searchSize), dimensions.width - 1),
+    Math.min(Math.max(0, movePoint[1] + searchSize * Math.random() - 0.5 * searchSize), dimensions.height - 1)
   ]
-
-  if (moveIndex === 0) {
-    polygon[polygon.length - 1] = polygon[0]
-  }
 
   const newEdgeScores = evaluatePolygon(newPolygon, getPixel, pixelBrightness, resolution)
 
@@ -258,14 +98,10 @@ function searchStep (polygon, getPixel, fn, resolution) {
   }
 }
 
-const maxFailCount = 300
-const maxSteps = 5000
-const framerate = 30
-
 function startJiggling(vnode) {
   console.log('Start jiggling!')
 
-  const getPixel = getBlurredPixel(vnode, 25)
+  const getPixel = getBlurredPixel(vnode)
   const fn = pixelBrightness
 
   let lastMessageTimestamp = 0
@@ -277,7 +113,7 @@ function startJiggling(vnode) {
   function jiggle () {
     step += 1
 
-    const results = searchStep(vnode.state.data.polygon, getPixel, fn, 5)
+    const results = searchStep(vnode.state.data.polygon, vnode.state.data.dimensions, getPixel, fn, SEARCH_SIZE, LINE_SAMPLE_RESOLUTION)
 
     if (results) {
       failCount = 0
@@ -297,20 +133,25 @@ function startJiggling(vnode) {
 
     if (step < maxSteps && failCount < maxFailCount) {
       setTimeout(jiggle, 1)
+    } else {
+      console.log('Done!')
+      m.redraw()
     }
   }
 
   jiggle()
 }
 
-function getBlurredPixel (vnode, blurSize = 5) {
+function getBlurredPixel (vnode) {
+  const blurRadius = vnode.state.data.blurRadius
+
   return function (x, y) {
-    const x1 = Math.max(0, Math.round(x - blurSize / 2))
-    const y1 = Math.max(0, Math.round(y - blurSize / 2))
+    const x1 = Math.max(0, Math.round(x - blurRadius / 2))
+    const y1 = Math.max(0, Math.round(y - blurRadius / 2))
 
-    const x2 = Math.min(vnode.state.data.dimensions.width, Math.round(x + blurSize / 2))
-    const y2 = Math.min(vnode.state.data.dimensions.height, Math.round(y + blurSize / 2))
-
+    const x2 = Math.min(vnode.state.data.dimensions.width - 1, Math.round(x + blurRadius / 2))
+    const y2 = Math.min(vnode.state.data.dimensions.height - 1, Math.round(y + blurRadius / 2))
+    // console.log(x1, y1, x2 - x1, y2 - y1)
     const imageData = vnode.state.data.imageContext.getImageData(x1, y1, x2 - x1, y2 - y1)
 
     const blurredPixelCount = imageData.data.length / 4
@@ -328,10 +169,11 @@ function getBlurredPixel (vnode, blurSize = 5) {
 }
 
 function updateEdgeScore(vnode, index) {
-  if (index >= 0 && index < vnode.state.data.polygon.length - 1) {
+  index = (index + vnode.state.data.polygon.length) % vnode.state.data.polygon.length
+  if (index >= 0 && index < vnode.state.data.polygon.length ) {
     const pointA = vnode.state.data.polygon[index]
-    const pointB = vnode.state.data.polygon[index + 1]
-    const getPixel = getBlurredPixel(vnode, 25)
+    const pointB = vnode.state.data.polygon[(index + 1) % vnode.state.data.polygon.length]
+    const getPixel = getBlurredPixel(vnode)
 
     const edgeScore = evaluateEdge(pointA, pointB, getPixel, pixelBrightness, 5)
     vnode.state.data.edgeScores[index] = edgeScore
@@ -348,8 +190,7 @@ const App = {
     },
     draggingIndex: -1,
     draggingMoved: false,
-    jiggling: false,
-    drawing: false
+    blurRadius: 25
   },
   view: (vnode) => ([
     m('header', [
@@ -364,9 +205,19 @@ const App = {
       }, 'Reset'),
       m('button', {
         onclick: (event) => {
-          startJiggling(vnode)
+          startJiggling(vnode, vnode.state.data.blurRadius)
         }
       }, 'Run!'),
+      m('input', {
+        type: 'range',
+        min: 1,
+        oninput: (event) => {
+          vnode.state.data.blurRadius = parseInt(event.target.value)
+          vnode.state.data.polygon.forEach((point, index) => {
+            updateEdgeScore(vnode, index)
+          })
+        }
+      }),
       m('div', {id: 'canvas-container'}, [
         m('img', {
           id: 'image',
@@ -418,6 +269,7 @@ const App = {
             ]
 
             vnode.state.data.polygon.push(point)
+            updateEdgeScore(vnode, vnode.state.data.polygon.length - 1)
             updateEdgeScore(vnode, vnode.state.data.polygon.length - 2)
           }
         }, [
@@ -429,7 +281,7 @@ const App = {
               y1: vnode.state.data.polygon[(index - 1 + vnode.state.data.polygon.length) % vnode.state.data.polygon.length][1],
               x2: point[0],
               y2: point[1],
-              stroke: getLineColor(vnode.state.data.edgeScores[index]),
+              stroke: getLineColor(vnode.state.data.edgeScores[(index - 1 + vnode.state.data.polygon.length) % vnode.state.data.polygon.length]),
               onmousedown: (event) => {
                 const point = [
                   event.offsetX,
@@ -461,6 +313,7 @@ const App = {
               onmouseup: (event) => {
                 if (!vnode.state.data.draggingMoved && vnode.state.data.draggingIndex === index) {
                   vnode.state.data.polygon.splice(index, 1)
+                  updateEdgeScore(vnode, index - 1)
                 }
 
                 vnode.state.data.draggingIndex = -1
